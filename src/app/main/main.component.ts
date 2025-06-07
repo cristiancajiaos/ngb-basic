1811ea7b8938e5e34ef77130cd75dde3adfc5020
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-06-06 20:14:03 -0400

    Agregado NgbAlert

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,15 @@
 import {Component, Input} from '@angular/core';
+import {NgbAlert} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [NgbAlert],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
 
   @Input() body: string = '';
+  @Input() dismissible: boolean = false;
 }
