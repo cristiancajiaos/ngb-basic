746675a0b598a3509e09a5d16814007bf234fd71
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-06-06 20:11:41 -0400

    Definido contenido

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+  @Input() body: string = '';
+}
